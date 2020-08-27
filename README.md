e69d2d9fc5ebe36b5859201fff392fd5ce453b0b
jfullerco <jonathan@jfuller.co>
2020-08-27 10:00:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-l862jl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-l862jl)
---
+++ comp/ToDo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+function ToDo() {
+  return(
+    <div>
+    <p><input type="checkbox" /></p>
+    <p><input type="checkbox" /></p>
+    <p><input type="checkbox"/></p>
+    <p><input type="checkbox" /></p>
+    </div>
+
+  )
+
+}
+
+export default ToDo
